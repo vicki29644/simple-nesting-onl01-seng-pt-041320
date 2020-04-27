dd0e3a9fcf4ba5402e2bc771dabc8527dcbb10c9
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-27 01:00:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,1 +1,56 @@
+def hopper
+    programmer_hash = {
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+    }
+    programmer_hash[:grace_hopper]
 
+end
+
+def alan_kay_is_known_for
+    # What combination of keys would you use to return the value of the :known_for key of :alan_kay
+    
+    programmer_hash = {
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+    }
+    programmer_hash[:alan_kay][:known_for]
+end
+
+def dennis_ritchies_language
+    programmer_hash = {
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+    }
+    programmer_hash[:dennis_ritchie][:languages][0]
+end
